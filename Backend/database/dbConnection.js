95799939b7b71d52593b8fa75872731841aad3f9
Ankush <ankushsultania42@gmail.com>
2024-07-01 22:33:40 +0530

    Readme File

--- Backend/database/dbConnection.js
+++ Backend/database/dbConnection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 export const dbConnection=()=>{
-    mongoose.connect(process.env.MONGO_URI,{
-        dbName:"CareCentral",
+    mongoose.connect('mongodb://127.0.0.1:27017',{
+        dbName:"Care_Central",
     }).then(()=>{
         console.log("Connected to database!");
     }).catch(err=>{
